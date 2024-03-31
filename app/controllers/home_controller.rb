f43e1cc3f82db57d22bb687570fbdb73d86c4568
Kerem Kahraman <kerem.kahraman001@gmail.com>
2024-03-31 16:33:36 +0300

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+require 'net/http'
+class HomeController < ApplicationController
+  def index
+    apikey = Rails.application.secrets[:openweathermap_api_key]
+    url = "https://api.openweathermap.org/data/2.5/weather?lat=41.0082&lon=28.9784&units=metric&appid=#{apikey}"
+    uri = URI(url)
+    res = Net::HTTP.get_response(uri)
+
+    @data = JSON.parse(res.body)
+  end
+end
