e4369851d027645e45353d8f9969b1514edd8e28
Kerem Kahraman <kerem.kahraman001@gmail.com>
2024-03-31 17:01:57 +0300

    tailwind added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ require 'net/http'
 class HomeController < ApplicationController
   def index
     apikey = Rails.application.secrets[:openweathermap_api_key]
+    puts "*"*200
+    puts apikey
     url = "https://api.openweathermap.org/data/2.5/weather?lat=41.0082&lon=28.9784&units=metric&appid=#{apikey}"
     uri = URI(url)
     res = Net::HTTP.get_response(uri)
