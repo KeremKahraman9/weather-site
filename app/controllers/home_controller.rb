61e6d6edbeeb03a1ed5c7f8829e6f93bbb5fc248
Kerem Kahraman <kerem.kahraman001@gmail.com>
2024-03-31 22:43:42 +0300

    view files beautify and flow improved

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,25 @@
 require 'net/http'
 class HomeController < ApplicationController
-  def index
-    apikey = Rails.application.secrets[:openweathermap_api_key]
-    puts "*"*200
-    puts apikey
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=41.0082&lon=28.9784&units=metric&appid=#{apikey}"
-    uri = URI(url)
-    res = Net::HTTP.get_response(uri)
+  def weather
+    if request.post?
+      session[:lat] = params[:lat]
+      session[:lon] = params[:lon]
 
-    @data = JSON.parse(res.body)
+      lat = params[:lat]
+      lon = params[:lon]
+    else
+      lat = session[:lat]
+      lon = session[:lon]
+    end
+
+    if lat.present? && lon.present?
+      apikey = Rails.application.secrets[:openweathermap_api_key]
+      url = "https://api.openweathermap.org/data/2.5/weather?lat=#{lat}&lon=#{lon}&units=metric&appid=#{apikey}"
+      uri = URI(url)
+      res = Net::HTTP.get_response(uri)
+      @data = JSON.parse(res.body)
+    else
+      @error = "Location data is missing."
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "home#index"
+  post 'weather', to: 'home#weather', as: 'weather'
+  get 'weather', to: 'home#weather'
 end
