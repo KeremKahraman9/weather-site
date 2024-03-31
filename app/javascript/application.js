87130bffaea0d969efebc9ae4bc400c5ce112bff
Kerem Kahraman <kerem.kahraman001@gmail.com>
2024-03-31 22:58:19 +0300

    some code blocks replaced and code refactoring

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.getElementById('getLocation').addEventListener('click', function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      document.getElementById('lat').value = position.coords.latitude;
+      document.getElementById('lon').value = position.coords.longitude;
+      document.getElementById('locationForm').submit();
+    }, function(error) {
+      alert("Location information could not be obtained: " + error.message);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+});
